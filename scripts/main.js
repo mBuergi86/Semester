03adadf6c45f87336865ea3b644c206973913045
mbuergi <m.buergi@linguala.com>
2024-06-27 08:21:09 +0200

    feat: ✨ Add "back to top" functionality in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,20 @@ import init from "./games.js";
 
 setLanguageByLocation();
 init();
+
+var backtop = document.getElementById("backtop");
+
+window.onscroll = function () {
+    if (window.pageYOffset > 300) {
+        backtop.classList.add("show");
+    } else {
+        backtop.classList.remove("show");
+    }
+};
+
+backtop.addEventListener("click", function () {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+    });
+});
