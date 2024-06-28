6b02453cdbc7d705a8a8ec7e36f32932c8225737
mbuergi <m.buergi@linguala.com>
2024-06-28 11:24:42 +0200

    refactor: ♻️ Improve back-to-top button behavior and add navigation toggle functionality

--- scripts/main.js
+++ scripts/main.js
@@ -4,19 +4,30 @@ import init from "./games.js";
 setLanguageByLocation();
 init();
 
-var backtop = document.getElementById("backtop");
+let backtop = document.getElementById("backtop");
+let navtoggle = document.querySelector(".nav-toggle");
+let menu_list = document.querySelector(".menu-list");
+let keyboard_arrows = document.querySelectorAll(".arrow_down");
 
 window.onscroll = function () {
-    if (window.pageYOffset > 300) {
-        backtop.classList.add("show");
-    } else {
-        backtop.classList.remove("show");
-    }
+  if (window.pageYOffset > 300) {
+    backtop.classList.add("show");
+  } else {
+    backtop.classList.remove("show");
+  }
 };
 
 backtop.addEventListener("click", function () {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-    });
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+navtoggle.addEventListener("click", function () {
+  menu_list.classList.toggle("open");
+  navtoggle.classList.toggle("open");
+  keyboard_arrows.forEach((arrow) => {
+    arrow.classList.toggle("open");
+  })
 });
