607a7a661472d3dd6859bd812244b5ceee8d66a2
mbuergi <m.buergi@linguala.com>
2024-07-01 15:46:01 +0200

    refactor: ♻️ Improve contact form submission with loading overlay

--- public/scripts/contact.js
+++ public/scripts/contact.js
@@ -1,9 +1,15 @@
 function initializeContactForm() {
   const form = document.getElementById("contact-form");
+  const loadingOverlay = document.getElementById("loading-overlay");
+  const body = document.body;
 
   if (form) {
     form.addEventListener("submit", async (e) => {
       e.preventDefault();
+
+      // Hinzufügen der Klasse "hidden" zum Verstecken des Formulars und Anzeigen des Lade-Overlays
+      loadingOverlay.classList.remove("hidden");
+      body.classList.add("disabled");
 
       const formData = new FormData(form);
       const formDataObject = Object.fromEntries(formData.entries());
@@ -17,10 +23,13 @@ function initializeContactForm() {
           body: JSON.stringify(formDataObject),
         });
 
+        loadingOverlay.classList.add("hidden");
+        body.classList.remove("disabled");
+
         if (response.ok) {
           const result = await response.json();
-          alert(result.message || "Nachricht erfolgreich gesendet!");
           form.reset();
+          window.location.href = "/thanks.html";
         } else {
           const errorData = await response.json();
           alert(
@@ -30,6 +39,10 @@ function initializeContactForm() {
         }
       } catch (error) {
         console.error("Error:", error);
+
+        loadingOverlay.classList.add("hidden");
+        body.classList.remove("disabled");
+
         alert(`Fehler beim Senden der Nachricht: ${error.message}`);
       }
     });
