0a6b6fee0cb0d02a9d722a46c010ebef3748b95c
mbuergi <m.buergi@linguala.com>
2024-07-01 09:00:56 +0200

    feat: ✨ initialize contact form on contact page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,18 @@
 import setLanguageByLocation from "./language.js"; // Importiert die Funktion zur Einstellung der Sprache basierend auf der Benutzerlocation
 import init from "./games.js"; // Importiert die Initialisierungsfunktion für Spiele
+import initializeContactForm from "./contact.js";
 
 setLanguageByLocation(); // Setzt die Sprache basierend auf der Benutzerlocation
 
 // Wenn die aktuelle URL "/game.html" ist
 if (window.location.pathname === "/game.html") {
   init(); // Initialisiert die Spiele
+}
+
+if (window.location.pathname === "/contact.html") {
+  document.addEventListener("DOMContentLoaded", function () {
+    initializeContactForm();
+  });
 }
 
 let scrollToTop = document.getElementById("backtop"); // Referenziert das Element mit der ID "backtop"
@@ -62,25 +69,55 @@ window.addEventListener("mousemove", function (e) {
 links.forEach((link) => {
   link.addEventListener("mouseenter", function () {
     // Neues Bild setzen
-    const newImage = document.createElement("img");
-    newImage.src = "./src/assets/icons/super_mario_hand.png";
-    newImage.alt = "hand";
+    const newImage = document.createElement("img"); // Neues Bild-Element erstellen
+    newImage.src = "./src/assets/icons/super_mario_hand.png"; // Bild-Source setzen
+    newImage.alt = "hand"; // Bild-Alt setzen
 
-    mushroomCursor.src = newImage.src;
-    mushroomCursor.alt = newImage.alt;
-    mushroomCursor.style.animationName = "none";
-    mushroomCursor.style.transform = "rotate(30deg)";
+    // Altes Bild entfernen und neues Bild hinzufügen
+    mushroomCursor.src = newImage.src; // Altes Bild-Source ersetzen
+    mushroomCursor.alt = newImage.alt; // Altes Bild-Alt ersetzen
+    mushroomCursor.style.animationName = "none"; // Animation deaktivieren
+    mushroomCursor.style.transform = "rotate(30deg)"; // Bewegung um 30° drehen
   });
 
   link.addEventListener("mouseleave", function () {
     // Altes Bild-Element erstellen und ersetzen
-    const oldImage = document.createElement("img");
-    oldImage.src = "./src/assets/icons/super_mario_mushroom.png";
-    oldImage.alt = "mushroom";
+    const oldImage = document.createElement("img"); // Altes Bild-Element erstellen
+    oldImage.src = "./src/assets/icons/super_mario_mushroom.png"; // Bild-Source setzen
+    oldImage.alt = "mushroom"; // Bild-Alt setzen
 
     // Neues Bild entfernen und altes Bild hinzufügen
-    mushroomCursor.src = oldImage.src;
-    mushroomCursor.alt = oldImage.alt;
-    mushroomCursor.style.animationName = "";
+    mushroomCursor.src = oldImage.src; // Altes Bild-Source ersetzen
+    mushroomCursor.alt = oldImage.alt; // Altes Bild-Alt ersetzen
+    mushroomCursor.style.animationName = ""; // Animation ausblenden
+  });
+});
+
+// Funktion, die beim Klicken auf das "mushroom"-Element ausgelöst wird
+window.addEventListener("mouseup", function (e) {
+  console.log("Mushroom clicked!");
+  // Wenn das "mushroom"-Element geklickt wird
+  // Neues Span-Element erstellen
+  const newSpan = document.createElement("span"); // Neues Span-Element erstellen
+  newSpan.style.display = "block"; // Span-Element anzeigen
+  newSpan.style.position = "fixed"; // Span-Element fixieren
+  newSpan.style.width = "2rem"; // Span-Element mit Breite von 2rem
+  newSpan.style.height = "2rem"; // Span-Element mit Hohe von 2rem
+  newSpan.style.backgroundColor = "white"; // Span-Element mit Hintergrundfarbe von weiss
+  newSpan.style.borderRadius = "50%"; // Span-Element mit Radius von 50%
+  newSpan.style.zIndex = "9999"; // Span-Element mit Index von 9999
+  newSpan.style.animationName = "mushroom-clicked"; // Animation benennen
+  newSpan.style.animationDuration = "0.2s"; // Animationdauer festlegen
+  newSpan.style.animationTimingFunction = "ease-in-out"; // Animation-Timing-Funktion festlegen
+  newSpan.style.left = e.clientX - 16 + "px"; // Span-Element an der X-Position des Mauszeigers setzen
+  newSpan.style.top = e.clientY - 11 + "px"; // Span-Element an der Y-Position des Mauszeigers setzen
+
+  // Span-Element hinzufügen
+  document.body.appendChild(newSpan); // Span-Element zum Body hinzufügen
+
+  // Span-Element nach der Animation entfernen
+  newSpan.addEventListener("animationend", function () {
+    newSpan.remove();
+    console.log("Span removed!");
   });
 });
